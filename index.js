19f09a070505da09ccb93057143cd24cfe17c8c9
Ikechi Michael <mykehell123@gmail.com>
2018-04-04 09:49:09 +0100

    feat: move explicit cluster script to index.js

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-const express = require('express')
+const cluster = require('cluster')
 const os = require('os')
+const express = require('express')
 const isPrime = require('./is-prime')
-const app = express()
 
-app.get('/', (req, res) => {
-    const primes = []
-    const max = Number(req.query.max) || 1000
-    for (let i = 1; i <= max; i++) {
-        if (isPrime(i)) primes.push(i)
+if (cluster.isMaster) {
+    const cpuCount = os.cpus().length
+    for (let i = 0; i < cpuCount; i++) {
+        cluster.fork()
     }
-    res.json(primes)
+}
+else {
+    const app = express()
+
+    app.get('/', (req, res) => {
+        const primes = []
+        const max = Number(req.query.max) || 1000
+        for (let i = 1; i <= max; i++) {
+            if (isPrime(i)) primes.push(i)
+        }
+        res.json(primes)
+    })
+    
+    app.get('/cpus', (req, res) => {
+        res.json(os.cpus())
+    })
+
+    app.get('/info', (req, res) => {
+        res.json({
+            id: cluster.worker.id
+        })
+    })
+
+    const port = process.env.PORT || 3030
+
+    app.listen(port)
+
+    console.log('app is running on port', port)
+}
+
+cluster.on('exit', (worker) => {
+    console.log('mayday! mayday! worker', worker.id, ' is no more!')
+    cluster.fork()
 })
-
-app.get('/cpus', (req, res) => {
-    res.json(os.cpus())
-})
-
-app.listen(process.env.POST || 3030)
-
-console.log('app is running!')
-
-module.exports = app
